0210c02aa5d22578ff23c87f429a62b5be4a76d7
Rajasree Nallu <rnallu@gmail.com>
2021-03-06 08:11:42 -0600

    Latest changes - 03-06

---
+++ app/src/main/java/com/example/fittimeapplication/AerobicExercise.java
@@ -0,0 +1,14 @@
+package com.example.fittimeapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AerobicExercise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aerobic_exercise);
+    }
+}
---
+++ app/src/main/java/com/example/fittimeapplication/ExcercisePage.java
@@ -0,0 +1,46 @@
+package com.example.fittimeapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ExcercisePage extends AppCompatActivity {
+
+    private Button AerobicExcercise;
+    private Button flexibilityExcercise;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_excercise_page);
+
+        initViews();
+
+        AerobicExcercise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ExcercisePage.this,AerobicExercise.class );
+                startActivity(intent);
+            }
+        });
+
+        flexibilityExcercise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ExcercisePage.this,FlexibilityExercise.class );
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void initViews() {
+        AerobicExcercise = findViewById(R.id.AerobicExcercise);
+        flexibilityExcercise = findViewById(R.id.FlexibilityExercise);
+    }
+
+
+}
